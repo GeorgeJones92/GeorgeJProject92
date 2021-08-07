fc4acaa85425cc80aa2e8dfd046a92acd409f5d2
GeorgeJones92 <31497271+GeorgeJones92@users.noreply.github.com>
2021-08-07 09:04:59 -0500

    Add files via upload
    Hubmap application I created when I worked with the City of Aberdeen, SD

---
+++ basemap-list.js
@@ -0,0 +1,101 @@
+var basemapList = [{	
+        title: '2018',
+        layerName: 'Imagery2018',
+        attribution: 'City of Aberdeen' 
+    }, {
+        title: '2014/2016',
+        layerName: 'Imagery20142016',
+        attribution: '&copy; City of Aberdeen (CC-BY-SA) / USDA, USGS (PD)'
+    }, {
+        title: 'Aug 2016',
+        layerName: 'NAIP2016',
+        attribution: 'USDA, USGS (PD)'
+    }, {
+        title: 'Sep 2014',
+        layerName: 'NAIP2014',
+        attribution: 'USDA, USGS (PD)'
+    }, {
+        title: 'Apr/Sep 2014',
+        layerName: 'Imagery2014',
+        attribution: '&copy; City of Aberdeen (CC-BY-SA) / USDA, USGS (PD)'
+    }, {
+        title: 'Jul 2012',
+        layerName: 'NAIP2012',
+        attribution: 'USDA, USGS (PD)'
+    }, {
+        title: 'Aug 2010',
+        layerName: 'NAIP2010',
+        attribution: 'USDA, USGS (PD)'
+    }, {
+        title: 'May 2009',
+        layerName: 'Aberdeen2009',
+        attribution: '&copy; City of Aberdeen (CC-BY-SA)'
+    }, {
+        title: 'Jul 2008',
+        layerName: 'NAIP2008',
+        attribution: 'USDA, USGS (PD)'
+    }, {
+        title: 'Jul 2006',
+        layerName: 'Imagery2006',
+        attribution: 'USDA, USGS (PD)'
+    }, {
+        title: 'Jul 2005',
+        layerName: 'Imagery2005',
+        attribution: 'USDA, USGS (PD)'
+    }, {
+        title: 'Jul 2004',
+        layerName: 'NAIP2004',
+        attribution: 'USDA, USGS (PD)'
+    }, {
+        title: 'May 2004',
+        layerName: 'Aberdeen2004',
+        attribution: '&copy; City of Aberdeen (CC-BY-SA)'
+    }, {
+        title: 'Jul 2003',
+        layerName: 'Imagery2003',
+        attribution: 'USDA, USGS (PD)'
+    }, {
+        title: 'Apr 1999',
+        layerName: 'Aberdeen1999',
+        attribution: '&copy; City of Aberdeen (CC-BY-SA)'
+    }, {
+        title: 'Sep 1997',
+        layerName: 'NASA1997',
+        attribution: 'NASA, USGS (PD)'
+    }, {
+        title: 'Apr 1994',
+        layerName: 'Aberdeen1994',
+        attribution: '&copy; City of Aberdeen (CC-BY-SA)'
+    }, {
+        title: 'Aug 1991',
+        layerName: 'NAPP1991',
+        attribution: 'NASA, USGS (PD)'
+    }, {
+        title: 'Jan 1987',
+        layerName: 'Aberdeen1987',
+        attribution: '&copy; City of Aberdeen (CC-BY-SA)'
+    }, {
+        title: 'Apr 1982',
+        layerName: 'Army1982',
+        attribution: 'NASA, USGS (PD)'
+    }, {
+        title: 'Oct 1979',
+        layerName: 'Aberdeen1979',
+        attribution: '&copy; City of Aberdeen (CC-BY-SA)'
+    }, {
+        title: 'Jul 1976',
+        layerName: 'NASA1976',
+        attribution: 'NASA, USGS (PD)'
+    }, {
+        title: 'May 1975',
+        layerName: 'Aberdeen1975',
+        attribution: '&copy; City of Aberdeen (CC-BY-SA)'
+    }, {
+        title: 'Jan 1958',
+        layerName: 'Army1958',
+        attribution: 'USGS (PD)'
+    }, {
+        title: 'Aug 1952',
+        layerName: 'Army1952',
+        attribution: 'US Army, USGS (PD)'
+    }];
---
+++ overlay-list.js
@@ -0,0 +1,126 @@
+var overlayList = [{
+  title: 'Addresses',
+  layerName: 'Addresses',
+  keywords: ['house', 'number'],
+  attribution: 'Extraterritorial addresses: Brown County'
+ }, {
+  title: 'City Council Districts',
+  layerName: 'CouncilDist',
+  keywords: ['council', 'commission', 'district'],
+  attribution: ''
+ }, {
+  title: 'Community Safety Zones',
+  layerName: 'CSZ',
+  keywords: ['police', 'sex', 'offender', 'covid', 'covid19', 'school', 'park'],
+  attribution: ''
+ }, {
+  title: 'Drone Areas',
+  layerName: 'DroneAreas',
+  keywords: ['fly', 'uav', 'air', 'aviation', 'ordinance'],
+  attribution: ''
+ }, {
+  title: 'Fire Hydrants',
+  layerName: 'FireHydrants',
+  keywords: ['hydrant', 'emergency'],
+  attribution: ''
+ }, {
+  title: 'Fire Stations',
+  layerName: 'fire_stations',
+  keywords: ['hydrant', 'fire', 'safety', 'emergency'],
+  attribution: ''
+ }, {
+  title: 'Flood Hazard',
+  layerName: 'FloodHazards',
+  keywords: ['fema', 'sfha', 'low', 'emergency'],
+  attribution: 'DFIRM: FEMA (PD)'
+ }, {
+  title: 'Garbage Route',
+  layerName: 'garbage_route',
+  keywords: ['sanitation', 'route', 'garbage', 'clean'],
+  attribution: ''
+ }, {
+  title: 'Historic Places',
+  layerName: 'HistoricDist',
+  keywords: ['shpo', 'landmark', 'building', 'district', 'old', 'preservation'],
+  attribution: ''
+ }, {
+  title: 'Jurisdictions',
+  layerName: 'Jurisdictions',
+  keywords: ['corporate', 'city', 'limit', 'boundary', 'three', '3', 'mile', 'administrative', 'police', 'power', 'authority', 'joint', 'zoning', 'three mile', 'three-mile', 'one', '1', 'administrative', 'joint', 'one mile', 'one-mile'],
+  attribution: ''
+ }, {
+  title: 'Lots',
+  layerName: 'AllLots',
+  keywords: ['parcel', 'lot', 'land', 'plat', 'survey', 'draw'],
+  attribution: ''
+ }, {
+  title: 'Parcels',
+  layerName: 'beacon_lots',
+  keywords: ['parcel', 'property', 'ownership'],
+  attribution: ''
+ }, {
+  title: 'Parks',
+  layerName: 'Parks',
+  keywords: ['park', 'recreation', 'play', 'swim', 'aquatic'],
+  attribution: ''
+ }, {
+  title: 'Public Land Survey System',
+  layerName: 'PLSS',
+  keywords: ['plss', 'lot', 'township', 'section', 'quarter', 'township', 'range'],
+  attribution: ''
+ }, {
+  title: 'Railways',
+  layerName: 'Rails',
+  keywords: ['rr', 'railroad', 'rail', 'road', 'train', 'track', 'bnsf', 'transport', 'transportation'],
+  attribution: ''
+ }, {
+  title: 'Roads',
+  layerName: 'Roads',
+  keywords: ['road', 'street', 'highway', 'way', 'transport', 'transportation'],
+  attribution: ''
+ }, {
+  title: 'Road Closures (repairs)',
+  layerName: 'road_closures',
+  keywords: ['closed', 'construction', 'transportation'],
+  attribution: ''
+ }, {
+  title: 'Schools',
+  layerName: 'Schools',
+  keywords: ['education', 'learning', 'child'],
+  attribution: ''
+ }, {
+  title: 'Snow Removal',
+  layerName: 'SnowRemoval',
+  keywords: ['road', 'street', 'highway', 'way', 'transport', 'transportation', 'ice', 'plow', 'storm'],
+  attribution: ''
+ },  {
+  title: 'Tax Increment Finance',
+  layerName: 'TIFDistrict',
+  keywords: ['tif', 'financing', 'special', 'district'],
+  attribution: ''
+ }, {
+  title: 'Topographic Contours',
+  layerName: 'DEM2010Contours2ft',
+  keywords: ['elevation', 'contour', 'height'],
+  attribution: 'Elevation: USDA/USGS (PD)'
+ }, {
+  title: 'Trails',
+  layerName: 'Trails',
+  keywords: ['trail', 'bike', 'biking', 'hike', 'hiking', 'recreation', 'play', 'park', 'way', 'path', 'road', 'transport', 'transportation'],
+  attribution: ''
+ }, {
+  title: 'Water Service Book Pages',
+  layerName: 'WaterServiceBook',
+  keywords: ['utilities', 'utility', 'potable', 'works'],
+  attribution: ''
+ }, {
+  title: 'Wetlands',
+  layerName: 'Wetlands',
+  keywords: ['usfws', 'wildlife', 'protect', 'flood', 'low'],
+  attribution: 'Wetlands: USFWS (PD)'
+ }, {
+  title: 'Zoning Districts',
+  layerName: 'Zoning',
+  keywords: ['zoning', 'use', 'special', 'planning', 'district', 'residential', 'industrial', 'commercial'],
+  attribution: ''
+ }];
